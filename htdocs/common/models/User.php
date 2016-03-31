1d7a547d5aa7661feadcd51611b7eaa7caafc75d
marcocruz <4acrom4@gmail.com>
2016-03-31 09:52:35 -0600

    Merge origin/master
    Conflicts:
    	README.md

--- htdocs/common/models/User.php
+++ htdocs/common/models/User.php
@@ -5,7 +5,9 @@ use Yii;
 use yii\base\NotSupportedException;
 use yii\behaviors\TimestampBehavior;
 use yii\db\ActiveRecord;
+use yii\Expression;
 use yii\web\IdentityInterface;
+use yii\helpers\Security;
 
 /**
  * User model
@@ -23,166 +25,210 @@ use yii\web\IdentityInterface;
  */
 class User extends ActiveRecord implements IdentityInterface
 {
-    const STATUS_DELETED = 0;
-    const STATUS_ACTIVE = 10;
+    const STATUS_ACTIVE = 1;
 
     /**
      * @inheritdoc
+     * Este método indica la tabla al cual estaremos trabajando
      */
     public static function tableName()
     {
-        return '{{%user}}';
+        return 'user';
     }
 
     /**
-     * @inheritdoc
-     */
+     * behaviors
+     * Con este método identificamos el tipod e comportamiento indicandole la clase a usar, después definimos los eventos la cual cada uno
+     * tiene sus propios atributos representados como campos en nuestra tabla; por último usamos la función now() ne mysql para la fecha actual.
+     
     public function behaviors()
     {
         return [
-            TimestampBehavior::className(),
+            'timestamp' => [
+                'class' => 'yii\behaviors\TimestampBehaviors',
+                    'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['created_at', 'updated_at'],
+                    ActiveRecord::EVENT_BEFORE_UPDATE => ['updated_at'],
+                    ],
+                'value' => new Expression('NOW()'),
+            ],
+        ];
+    }*/
+    
+    public function behaviors()
+    {
+        return [
+            'timestamp' => [
+                'class' => 'yii\behaviors\TimestampBehavior',
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['created_at', 'updated_at'],
+                    ActiveRecord::EVENT_BEFORE_UPDATE => ['updated_at'],
+                ],
+                'value' => new Expression('NOW()'),
+            ],
         ];
     }
 
     /**
-     * @inheritdoc
+     * rules of validation
+     * Se especifican las reglas de validaciones de cada uno de los atributos
      */
     public function rules()
     {
         return [
-            ['status', 'default', 'value' => self::STATUS_ACTIVE],
-            ['status', 'in', 'range' => [self::STATUS_ACTIVE, self::STATUS_DELETED]],
+            ['status_id', 'default', 'value' => self::STATUS_ACTIVE],
+            
+            ['rol_id', 'default', 'value' => 1],
+            
+            ['type_user_id', 'default', 'value' => 1],
+            
+            ['username', 'filter', 'filter' => 'trim'],
+            ['username', 'required'],
+            ['username', 'unique'],
+            ['username', 'string', 'min' => 2, 'max' => 255],
+            
+            ['email', 'filter', 'filter' => 'trim'],
+            ['email', 'required'],
+            ['email', 'email'],
+            ['email', 'unique']
         ];
     }
 
+    
+    /**
+     * Labels of the atributs of he model
+     */
+    
+    public function attributeLabels() {
+        return[
+            /* others labels of atributs */
+        ];
+    }
+    
+    /**
+     * @findIndentity
+     */
+    
+    public static function findIdentity($id){
+        return static::findOne(['Id' => $id, 'estatus_id' => self::STATUS_ACTIVE]);
+    }
+     
+    
     /**
      * @inheritdoc
      */
-    public static function findIdentity($id)
-    {
-        return static::findOne(['id' => $id, 'status' => self::STATUS_ACTIVE]);
+   
+    public static function findIdentityByAccessToken($token, $type = null) {
+        throw new NotSupportedException('"findIdentityAccessToken" is not implemented.');
     }
-
+    
     /**
-     * @inheritdoc
-     */
-    public static function findIdentityByAccessToken($token, $type = null)
-    {
-        throw new NotSupportedException('"findIdentityByAccessToken" is not implemented.');
-    }
-
-    /**
-     * Finds user by username
-     *
+     * Encuentra usuario por username
      * @param string $username
      * @return static|null
      */
-    public static function findByUsername($username)
-    {
-        return static::findOne(['username' => $username, 'status' => self::STATUS_ACTIVE]);
+    
+    public static function findByUsername($username){
+        return static::findOne(['username' => $username, 'status_id' => self::STATUS_ACTIVE]);
     }
-
+    
     /**
-     * Finds user by password reset token
-     *
-     * @param string $token password reset token
+     *Encuentra usuario por clave de restablecimiento de password
+     * @param string $token clave de restablecimiento de password
      * @return static|null
      */
-    public static function findByPasswordResetToken($token)
-    {
-        if (!static::isPasswordResetTokenValid($token)) {
+    
+    public static function findByPasswordResetToken($token){
+        if(!static::isPasswordResetTokenValid($token)){
             return null;
         }
-
-        return static::findOne([
+        return static::findObne([
             'password_reset_token' => $token,
-            'status' => self::STATUS_ACTIVE,
+            'status_id' => self::STATUS_ACTIVE,
         ]);
     }
-
+    
     /**
-     * Finds out if password reset token is valid
-     *
-     * @param string $token password reset token
-     * @return boolean
+     * Determina si la clave de restablecimiento de password es valida
+     * 
+     * @param strin $token clave de restablecimiento de password
+     * @retur boolean
      */
-    public static function isPasswordResetTokenValid($token)
-    {
-        if (empty($token)) {
+    
+    public static function isPasswordResetTokenValid($token){
+        if(empty($token)){
             return false;
         }
-
-        $timestamp = (int) substr($token, strrpos($token, '_') + 1);
         $expire = Yii::$app->params['user.passwordResetTokenExpire'];
+        $parts = explode('_',$token);
+        $timestamp = (int) end($parts);
         return $timestamp + $expire >= time();
     }
-
+    
     /**
-     * @inheritdoc
+     * @getId
      */
-    public function getId()
-    {
+    
+    public function getId() {
         return $this->getPrimaryKey();
     }
-
+    
     /**
-     * @inheritdoc
+     * @getAutKey
      */
-    public function getAuthKey()
-    {
+    
+    public function getAuthKey() {
         return $this->auth_key;
     }
-
+    
     /**
-     * @inheritdoc
+     * @validateAuthKey
      */
-    public function validateAuthKey($authKey)
-    {
+    
+    public function validateAuthKey($authKey) {
         return $this->getAuthKey() === $authKey;
     }
-
+    
     /**
-     * Validates password
-     *
-     * @param string $password password to validate
-     * @return boolean if password provided is valid for current user
+     * Valida password
+     * 
+     * @param string $password password a validar
+     * @return boolean si el password provista es válida par el usuario actual
      */
-    public function validatePassword($password)
-    {
-        return Yii::$app->security->validatePassword($password, $this->password_hash);
+    
+    public function validatePassword($password){
+        return Yii::$app->security->validatePassword($password, $this->password-has);
     }
-
+    
     /**
-     * Generates password hash from password and sets it to the model
-     *
+     * Genera has de psswor a partir de password y la establece en el modelo
+     * 
      * @param string $password
      */
-    public function setPassword($password)
-    {
+    
+    public function setPassword($password){
         $this->password_hash = Yii::$app->security->generatePasswordHash($password);
     }
-
+    
     /**
-     * Generates "remember me" authentication key
+     * Genera clave de autentificación "recuerdame"
      */
-    public function generateAuthKey()
-    {
+    public function generateAuthkey(){
         $this->auth_key = Yii::$app->security->generateRandomString();
     }
-
+    
     /**
-     * Generates new password reset token
+     * Genera nueva clave de reestablecimiento de password
      */
-    public function generatePasswordResetToken()
-    {
-        $this->password_reset_token = Yii::$app->security->generateRandomString() . '_' . time();
+    public function generatePasswordResetToken(){
+        $this->password_reset_token = Yii::$app->security->generateRandomString().'_'.time();
     }
-
+    
     /**
-     * Removes password reset token
+     * Remueve clave de reestablecimiento de password
      */
-    public function removePasswordResetToken()
-    {
+    
+    public function removePasswordResetToken() {
         $this->password_reset_token = null;
     }
 }
